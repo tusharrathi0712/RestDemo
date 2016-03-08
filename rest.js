79dda2822c9f109e8cb13ec2f91829d54f54456f
tusharrathi0712 <tusharrathi0712@gmail.com>
2016-03-08 21:00:29 +0530

    Update rest.js

--- rest.js
+++ rest.js
@@ -17,11 +17,13 @@ route.get('/users', function(req, res) {
 	};
 	connection.query("select * from user1", function(err, rows, fields) {
 		if (rows.length !== 0) {
-			users.error = 0;
+			users.error = 200;
 			users.user = rows;
 			res.json(users);
 		} else {
-			res.sendStatus(404);
+			users.error = 200;
+			users.user = "No data found....";
+			res.json(users);
 		}
 	});
 });
@@ -35,17 +37,19 @@ route.get('/users/:id', function(req, res) {
 
 	connection.query("select * from user1 where id=?",[id], function(err, rows, fields) {
 		 if (rows.length !== 0) {
-			users.error = 0;
+			users.error = 200;
 			users.user = rows;
 			res.json(users);
 		 }else { 
-			 res.sendStatus(404); 
+			 users.error = 200;
+			 users.user = "No user found with requested id....";
+			 res.json(users);
 		 }	 
 	});
 	
 });
 
-route.post('/users',function(req,res){
+route.put('/users',function(req,res){
     var Firstname = req.body.firstname;
     var Lastname = req.body.lastname;
     var Email = req.body.email;
@@ -55,17 +59,28 @@ route.post('/users',function(req,res){
         "error":1,
         "user":""
     };
- 
+    if(!!Firstname && !!Lastname && !!Email && !!id){
         connection.query("INSERT INTO user1 VALUES(?,?,?,?)",[Firstname,Lastname,Email,id],function(err, rows, fields){
             if(!!err){   
-                res.sendStatus(500);
+            	users.error = 500;
+    			users.user = "Internal Server Problem";
+    			
             }else{
-               	res.sendStatus(201);
-            }           
+            	users.error = 201;
+    			users.user = "User inserted into database";
+            } 
+            res.json(users);
         });
+    }else{
+    	users.error = 422;
+		users.user = "Missing data in Json....";
+            res.json(users);
+        }
+
+       
 });
 
-route.put('/users',function(req,res){
+route.post('/users',function(req,res){
     
     var Firstname = req.body.firstname;
     var Lastname = req.body.lastname;
@@ -79,13 +94,18 @@ route.put('/users',function(req,res){
     if(!!Firstname && !!Lastname && !!Email && !!id){
         connection.query("UPDATE user1 SET firstname=?, lastname=?,email=? WHERE id=?",[Firstname,Lastname,Email,id],function(err, rows, fields){
             if(!!err){
-            	res.sendStatus(404);
+            	users.error = 404;
+    			users.user = "No data found...";
+    			res.json(users);
             }else{
-            	res.sendStatus(201);
+            	users.error = 201;
+    			users.user = "User inserted into database";
+    			res.json(users);
             }
             
         });
     }else{
+    	users.error = 422;
     	users.user = "Please provide all required data (i.e : firstname, lastname, email)";
         res.json(users);
     }
@@ -100,13 +120,17 @@ route.delete('/users/:id',function(req,res){
     if(!!id){
         connection.query("DELETE FROM user1 WHERE id=?",[id],function(err, rows, fields){
             if(!!err){             
-                res.sendStatus(404);
+            	users.error = 404;
+    			users.user = "No user found...";
+    			res.json(users);
             }else{           
-            	res.sendStatus(200);
-            }
-            
+            	users.error = 201;
+    			users.user = "User deleted successfully";
+    			res.json(users);
+            }           
         });
     }else{
+    	users.error = 400;
     	users.user = "Please provide all required data (i.e : id )";
         res.json(users);
     }
