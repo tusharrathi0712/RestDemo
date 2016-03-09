fa710a872c1be084cd90169c065f2febc27633c0
tusharrathi0712 <tusharrathi0712@gmail.com>
2016-03-09 13:44:15 +0530

    Update rest.js

--- rest.js
+++ rest.js
@@ -12,17 +12,17 @@ var connection = mysql.createConnection({
 
 route.get('/users', function(req, res) {
 	var users = {
-		"error" : 1,
+		"status" : 1,
 		"user" : ""
 	};
 	connection.query("select * from user1", function(err, rows, fields) {
 		if (rows.length !== 0) {
-			users.error = 200;
+			users.status = 200;
 			users.user = rows;
 			res.json(users);
 		} else {
-			users.error = 200;
-			users.user = "No data found....";
+			users.status = 200;
+			users.user = "No User found....";
 			res.json(users);
 		}
 	});
@@ -31,17 +31,18 @@ route.get('/users', function(req, res) {
 route.get('/users/:id', function(req, res) {
 	var id=req.params.id;
 	var users = {
-		"error" : 1,
+		"status" : 1,
 		"user" : ""
 	};
 
 	connection.query("select * from user1 where id=?",[id], function(err, rows, fields) {
 		 if (rows.length !== 0) {
-			users.error = 200;
+			users.status = 200;
 			users.user = rows;
 			res.json(users);
 		 }else { 
-			 users.error = 200;
+			 res.status(404);
+			 users.status = 404;
 			 users.user = "No user found with requested id....";
 			 res.json(users);
 		 }	 
@@ -56,23 +57,31 @@ route.put('/users',function(req,res){
     var id = req.body.id;
     
     var users = {
-        "error":1,
+        "status":1,
         "user":""
     };
+    
     if(!!Firstname && !!Lastname && !!Email && !!id){
+    	
+    	
         connection.query("INSERT INTO user1 VALUES(?,?,?,?)",[Firstname,Lastname,Email,id],function(err, rows, fields){
-            if(!!err){   
-            	users.error = 500;
+            if(!!err){  
+            	res.status(500);
+            	users.status = 500;
     			users.user = "Internal Server Problem";
     			
             }else{
-            	users.error = 201;
+            	res.status(201);
+            	users.status = 201;
     			users.user = "User inserted into database";
             } 
-            res.json(users);
+            res.json(users);	
+       
         });
+        
     }else{
-    	users.error = 422;
+    	res.status(422);
+    	users.status = 422;
 		users.user = "Missing data in Json....";
             res.json(users);
         }
@@ -88,25 +97,28 @@ route.post('/users',function(req,res){
     var id = req.body.id;
     
     var users = {
-        "error":1,
+        "status":1,
         "user":""
-    };
+    };   
+    
     if(!!Firstname && !!Lastname && !!Email && !!id){
-        connection.query("UPDATE user1 SET firstname=?, lastname=?,email=? WHERE id=?",[Firstname,Lastname,Email,id],function(err, rows, fields){
+        connection.query("UPDATE user1 SET firstname=?, lastname=?, email=? WHERE id=?",[id,Firstname,Lastname,Email,id],function(err, rows, fields){
             if(!!err){
-            	users.error = 404;
+            	res.status(404);
+            	users.status = 404;
     			users.user = "No data found...";
     			res.json(users);
             }else{
-            	users.error = 201;
-    			users.user = "User inserted into database";
+            	users.status = 200;
+    			users.user = "User updated into database";
     			res.json(users);
             }
             
         });
     }else{
-    	users.error = 422;
-    	users.user = "Please provide all required data (i.e : firstname, lastname, email)";
+    	res.status(422);
+    	users.status = 422;
+    	users.user = "Missing data in json";
         res.json(users);
     }
 });
@@ -114,26 +126,23 @@ route.post('/users',function(req,res){
 route.delete('/users/:id',function(req,res){
     var id = req.params.id;
     var users = {
-        "error":1,
+        "status":1,
         "user":""
     };
-    if(!!id){
+   
         connection.query("DELETE FROM user1 WHERE id=?",[id],function(err, rows, fields){
-            if(!!err){             
-            	users.error = 404;
+            if(!!err){   
+            	res.status(404);
+            	users.status = 404;
     			users.user = "No user found...";
     			res.json(users);
             }else{           
-            	users.error = 201;
+            	users.status = 200;
     			users.user = "User deleted successfully";
     			res.json(users);
             }           
         });
-    }else{
-    	users.error = 400;
-    	users.user = "Please provide all required data (i.e : id )";
-        res.json(users);
-    }
+    
 });
 
 module.exports = route;
