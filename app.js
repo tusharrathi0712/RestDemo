05348f44fdd1f6b2c016dba89c4837bcbbf494ad
tusharrathi0712 <tusharrathi0712@gmail.com>
2016-03-15 17:06:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,26 +9,5 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/', route);
-//app.use('/users', route);
-//app.use('/users/:id', route);
-
-app.use(function(req, res, next) {
-	var err = new Error('Resource Not Found');
-	err.status = 404;
-	next(err);
-});
-
-app.use(function(err, req, res, next) {
-	
-	res.sendStatus(400);
-	/*res.sendStatus();
-	
-	//res.status(err.status || 500);
-	
-	res.render('error', {
-		message : err.message,
-		error : err
-	});*/
-});
 
 module.exports = app;
